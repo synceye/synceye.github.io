347e7b353e8d9471e7ed132034d429c669b1648a
SyncEye <50116007+DamjanPetrovicki@users.noreply.github.com>
2020-03-16 13:23:48 +0100

    Update portfolio.js

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -193,7 +193,7 @@ GitHub: https://github.com/Lumm1t/obnoxious.club
 
                     clearCursor();
 
-                    writeLine(["<i style='color: #F62459'>sync.eye#9999</i>"], 120, 500, function () {
+                    writeLine(["<i style='color: #F62459'>sync.eye#2817</i>"], 120, 500, function () {
 
                         timeouts.push(setTimeout(function () {
 
