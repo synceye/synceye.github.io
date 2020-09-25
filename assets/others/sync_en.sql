f6fdc1ca864cdd3e1f71b9b214ff6393facef43e
SyncEye <50116007+DamjanPetrovicki@users.noreply.github.com>
2020-09-25 09:03:49 +0200

    Update sync_en.sql

--- assets/others/sync_en.sql
+++ assets/others/sync_en.sql
@@ -1023,8 +1023,8 @@ INSERT INTO `addon_inventory` (name, label, shared) VALUES
 	('society_mafia', 'mafia', 1)
 ;
 
-INSERT INTO `jobs` (name, label) VALUES
-	('mafia','Mafia')
+INSERT INTO `jobs` (name, label, whitelisted) VALUES
+	('mafia','Mafia', 1)
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
