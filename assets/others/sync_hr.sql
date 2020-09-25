e26b4c9d77ae4a0508322d6794baedbefb68517e
SyncEye <50116007+DamjanPetrovicki@users.noreply.github.com>
2020-09-25 09:03:06 +0200

    Update sync_hr.sql

--- assets/others/sync_hr.sql
+++ assets/others/sync_hr.sql
@@ -1024,8 +1024,8 @@ INSERT INTO `addon_inventory` (name, label, shared) VALUES
 	('society_mafia', 'mafia', 1)
 ;
 
-INSERT INTO `jobs` (name, label) VALUES
-	('mafia','Mafija')
+INSERT INTO `jobs` (name, label, whitelisted) VALUES
+	('mafia','Mafija', 1)
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
