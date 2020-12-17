e9b54ed6843def95c75cebc64545f59135289196
SyncEye <50116007+DamjanPetrovicki@users.noreply.github.com>
2020-12-17 15:20:55 +0100

    Update sync_en.sql

--- assets/others/sync_en.sql
+++ assets/others/sync_en.sql
@@ -89,27 +89,6 @@ CREATE TABLE IF NOT EXISTS `trunk_inventory` (
   UNIQUE KEY `plate` (`plate`)
 );
 
-CREATE TABLE `shops` (
-	`id` int NOT NULL AUTO_INCREMENT,
-	`store` varchar(100) NOT NULL,
-	`item` varchar(100) NOT NULL,
-	`price` int NOT NULL,
-
-	PRIMARY KEY (`id`)
-);
-
-INSERT INTO `shops` (store, item, price) VALUES
-	('TwentyFourSeven','bread',30),
-	('TwentyFourSeven','water',15),
-	('RobsLiquor','bread',30),
-	('RobsLiquor','water',15),
-	('LTDgasoline','bread',30),
-	('LTDgasoline','water',15),
-	('TwentyFourSeven','fixtool',1500),
-	('RobsLiquor','fixtool',1500),
-	('LTDgasoline','fixtool',1500)
-;
-
 ALTER TABLE `users` ADD COLUMN `phone_number` VARCHAR(10) NULL;
 
 
